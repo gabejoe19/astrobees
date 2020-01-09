b543906f06878658dec0c2e195e9665b65f80604
sidfoe <sidfoe2125@gmail.com>
2020-01-09 17:44:22 -0500

    punnett code

--- AstroBeesUnity/Assets/Scripts/PotBehaviour.cs
+++ AstroBeesUnity/Assets/Scripts/PotBehaviour.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PotBehaviour : MonoBehaviour
 {
-    public int[][] colorTraits;
-    public int[][] stemTraits;
-    public int[][] petalTraits;
-    public int[][] thornsTraits;
+    public string[,] colorTraits;
+    public string[,] stemTraits;
+    public string[,] petalTraits;
+    public string[,] thornsTraits;
+
+    public string[,] prevColor;
+    public string[,] prevStem;
+    public string[,] prevPetal;
+    public string[,] prevThorns;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +24,53 @@ public class PotBehaviour : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void PunnettSquare(string[,] traits, string[,] prevTraits)
+    {
+        string[,] square = new string[2, 2];
+        string[,] results = new string[4, 2];
+        
+        //first set of traits
+        square[0, 0] = traits[0, 0];
+        square[0, 1] = traits[0, 1];
+        //second set of traits
+        square[1, 0] = prevTraits[0, 0];
+        square[1, 1] = prevTraits[0, 1];
+
+        //result 1
+        results[0, 0] = square[0, 0];
+        results[0, 1] = square[1, 0];
+        //result 2
+        results[1, 0] = square[0, 1];
+        results[1, 1] = square[1, 0];
+        //result 3
+        results[2, 0] = square[0, 0];
+        results[2, 1] = square[1, 1];
+        //result 4
+        results[3, 0] = square[0, 1];
+        results[3, 1] = square[1, 1];
+
+        int winner = Random.Range(0, 100);
+
+        if(winner <= 25)
+        {
+
+        }
+        
+        else if(winner <= 50 && winner > 25)
+        {
+
+        }
+
+        else if(winner <= 75 && winner > 50)
+        {
+
+        }
+
+        else if(winner <= 100 && winner > 75)
+        {
+            
+        }
     }
 }
